8129755d255b26a7c1f4f47b078a38313caa6c10
Idelfonso Joas <joas@herosoftware.com.br>
2022-09-24 22:26:40 -0300

    add user list provider

---
+++ lib/provider/user_list_provider.dart
@@ -0,0 +1,32 @@
+import 'package:crud_user_registration/sql_helper.dart';
+import 'package:flutter/cupertino.dart';
+
+import '../models/user.dart';
+
+class UserListProvider with ChangeNotifier {
+  final db = UserDbProvider();
+
+  Future<List<User>> getUserList() async {
+    List<User> _userList = await db.fetchUsers();
+
+    return _userList;
+  }
+
+  Future<void> addUser(User user) async {
+    await db.addUser(user);
+
+    notifyListeners();
+  }
+
+  Future<void> updateUser(User user) async {
+    await db.updateUser(user.id!, user);
+
+    notifyListeners();
+  }
+
+  Future<void> deleteUser(int userId) async {
+    await db.deleteUser(userId);
+
+    notifyListeners();
+  }
+}
